4e6bcf3a0988055f72c8a673d9c04c74ea5abbb5
成龙生 <1536463948@qq.com>
2022-06-21 18:31:58 +0800

    添加模糊查询页面后请求逻辑的判断

--- thymeleaf/src/main/java/person/cls/thymeleaf/fruit/servlets/IndexServlet.java
+++ thymeleaf/src/main/java/person/cls/thymeleaf/fruit/servlets/IndexServlet.java
@@ -25,22 +25,63 @@ public class IndexServlet extends ViewBaseServlet {
     private final FruitDAOImpl fruitDAO = new FruitDAOImpl();
 
     @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        int pageNo = 1;
+        req.setCharacterEncoding("UTF-8");
+        HttpSession session = req.getSession();
 
+        int pageNo = 1;
+        int size;
+        List<Fruit> fruitList;
+        String keyword;
+
+        // 请求方式判断标识
+        String operation = req.getParameter("operation");
+
+        // 当前是在第几页
         String pageNoStr = req.getParameter("pageNo");
         if (!StringUtils.isEmpty(pageNoStr)) {
             pageNo = Integer.parseInt(pageNoStr);
         }
 
-        List<Fruit> fruitList = fruitDAO.getFruitList(pageNo);
-        int size = fruitDAO.getFruitList().size();
+        /*
+        如果operation是null则是get请求，
+        否则就是post请求
+         */
+        if (!StringUtils.isEmpty(operation) && "search".equals(operation)) {
+            // post请求处理表单
+            keyword = req.getParameter("keyword");
+            // 用户空输入判断
+            if (StringUtils.isEmpty(keyword)) {
+                keyword = "";
+                size = fruitDAO.getFruitList().size();
+                fruitList = fruitDAO.getFruitList(pageNo);
+            } else {
+                size = fruitDAO.getFruitList(keyword).size();
+                fruitList = fruitDAO.getFruitList(keyword, pageNo);
+            }
+            // get请求.地址栏直接输入网址，上一页下一页
+        } else {
+            keyword = (String) session.getAttribute("keyword");
+            if (StringUtils.isEmpty(keyword)) {
+                size = fruitDAO.getFruitList().size();
+                fruitList = fruitDAO.getFruitList(pageNo);
+            } else {
+                size = fruitDAO.getFruitList(keyword).size();
+                fruitList = fruitDAO.getFruitList(keyword, pageNo);
+            }
+        }
 
+        // 页面数量
         int pageCount = (size + 5 - 1) / 5;
 
-        HttpSession session = req.getSession();
         session.setAttribute("fruitList", fruitList);
+        session.setAttribute("keyword", keyword);
         session.setAttribute("pageNo", pageNo);
         session.setAttribute("pageCount", pageCount);
 
