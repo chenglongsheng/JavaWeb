39c5effa631fc0eda06957f7c01e13e4158b4463
成龙生 <1536463948@qq.com>
2022-06-26 09:26:34 +0800

    解析XML文件，生成对应的controller实例

--- mvc/src/main/java/person/cls/mvc/myssm/myspringmvc/DispatchServlet.java
+++ mvc/src/main/java/person/cls/mvc/myssm/myspringmvc/DispatchServlet.java
@@ -1,12 +1,23 @@
 package person.cls.mvc.myssm.myspringmvc;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import person.cls.mvc.myssm.util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @description: Servlet调用中心（核心控制器）
@@ -16,6 +27,39 @@ import java.io.IOException;
  */
 @WebServlet("*.do")
 public class DispatchServlet extends ViewBaseServlet {
+
+    private Map<String, Object> beanMap = new HashMap<>();
+
+    /**
+     * 解析XML文件，通过反射生成对应的controller实例，然后存放到beanMap容器中
+     */
+    public DispatchServlet() {
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream("applicationContext.xml");
+            // 1.创建DocumentBuilderFactory对象
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            // 2.DocumentBuilder对象
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            // 3.创建Document对象
+            Document document = documentBuilder.parse(is);
+
+            // 4.获取所有的bean节点
+            NodeList nodeList = document.getElementsByTagName("bean");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node beanNode = nodeList.item(i);
+                if (beanNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element beanElement = (Element) beanNode;
+                    String beanId = beanElement.getAttribute("id");
+                    String beanClass = beanElement.getAttribute("class");
+                    Object beanObj = Class.forName(beanClass).newInstance();
+                    beanMap.put(beanId, beanObj);
+                }
+            }
+        } catch (ParserConfigurationException | IOException | SAXException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 设置编码
