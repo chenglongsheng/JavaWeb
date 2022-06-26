46659d4b952d8ff6b955ae9c6b65d28473fbf08c
成龙生 <1536463948@qq.com>
2022-06-26 21:05:20 +0800

    改成controller类，抽取出请求参数赋值操作

--- mvc/src/main/java/person/cls/mvc/fruit/controller/FruitController.java
+++ mvc/src/main/java/person/cls/mvc/fruit/controller/FruitController.java
@@ -10,32 +10,24 @@ import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
- * @description: 合并增删改查的servlet
+ * @description: 将servlet抽取出，变成一个纯类
  * @author: CLS
  * @date: 2022-06-25-9:41
- * @version: 1.0
+ * @version: 2.0
  */
 public class FruitController {
 
     private final FruitDAOImpl fruitDAO = new FruitDAOImpl();
 
-    protected String index(HttpServletRequest request) throws ServletException{
+    protected String index(Integer pageNo, String keyword, String operation, HttpServletRequest request) throws ServletException {
 
         HttpSession session = request.getSession();
 
-        int pageNo = 1;
-        int size;
-        List<Fruit> fruitList;
-        String keyword;
-
-        // 请求方式判断标识
-        String operation = request.getParameter("operation");
-
-        // 当前是在第几页
-        String pageNoStr = request.getParameter("pageNo");
-        if (!StringUtils.isEmpty(pageNoStr)) {
-            pageNo = Integer.parseInt(pageNoStr);
+        if (pageNo == null) {
+            pageNo = 1;
         }
+        List<Fruit> fruitList;
+        int size;
 
         /*
         如果operation是null则是get请求，
@@ -43,7 +35,6 @@ public class FruitController {
          */
         if (!StringUtils.isEmpty(operation) && "search".equals(operation)) {
             // post请求处理表单
-            keyword = request.getParameter("keyword");
             // 用户空输入判断
             if (StringUtils.isEmpty(keyword)) {
                 keyword = "";
@@ -72,66 +63,34 @@ public class FruitController {
         session.setAttribute("keyword", keyword);
         session.setAttribute("pageNo", pageNo);
         session.setAttribute("pageCount", pageCount);
-
-//        processTemplate("index", request, response);
-
         return "index";
 
     }
 
-    protected String add(HttpServletRequest request) throws ServletException {
-
-        String fname = request.getParameter("fname");
-        Integer price = Integer.parseInt(request.getParameter("price"));
-        Integer fcount = Integer.parseInt(request.getParameter("fcount"));
-        String remark = request.getParameter("remark");
-
+    protected String add(String fname, Integer price, Integer fcount, String remark) {
         Fruit fruit = new Fruit(0, fname, price, fcount, remark);
         fruitDAO.addFruit(fruit);
-
-//        response.sendRedirect("fruit.do");
         return "redirect:fruit.do";
-
     }
 
-    protected String del(HttpServletRequest request) throws ServletException {
-        int fid = Integer.parseInt(request.getParameter("fid"));
+    protected String del(Integer fid) throws ServletException {
         fruitDAO.delFruitById(fid);
-//        response.sendRedirect("fruit.do");
         return "redirect:fruit.do";
     }
 
-    protected String edit(HttpServletRequest request) throws ServletException {
-
-        String fid = request.getParameter("fid");
-        if (!StringUtils.isEmpty(fid)) {
-            int id = Integer.parseInt(fid);
-            Fruit fruitInfo = fruitDAO.getFruitInfo(id);
+    protected String edit(HttpServletRequest request, Integer fid) {
+        if (fid != null) {
+            Fruit fruitInfo = fruitDAO.getFruitInfo(fid);
             request.setAttribute("fruit", fruitInfo);
-//            processTemplate("edit", request, response);
             return "edit";
         }
         return "error";
     }
 
-    protected String update(HttpServletRequest request) throws ServletException {
-
-        String fidStr = request.getParameter("fid");
-        Integer fid = Integer.parseInt(fidStr);
-        String fname = request.getParameter("fname");
-        String priceStr = request.getParameter("price");
-        Integer price = Integer.parseInt(priceStr);
-        String fcountStr = request.getParameter("fcount");
-        Integer fcount = Integer.parseInt(fcountStr);
-        String remark = request.getParameter("remark");
-
+    protected String update(Integer fid, String fname, Integer price, Integer fcount, String remark) {
         Fruit fruit = new Fruit(fid, fname, price, fcount, remark);
         fruitDAO.updateFruitById(fruit);
-
-        // 浏览器重定向
-//        response.sendRedirect("fruit.do");
         return "redirect:fruit.do";
-
     }
 
 }
