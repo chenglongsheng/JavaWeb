7a741551fbff222d91c685d0a692b38fc76f0b8d
成龙生 <1536463948@qq.com>
2022-07-05 16:29:38 +0800

    配置使得thymeleaf语法生效

---
+++ QQZone/src/main/java/person/cls/qqzone/myssm/controller/PageController.java
@@ -0,0 +1,15 @@
+package person.cls.qqzone.myssm.controller;
+
+/**
+ * @description: 页面跳转经过thymeleaf模板引擎
+ * @author: CLS
+ * @date: 2022-07-05-16:17
+ * @version: 1.0
+ */
+public class PageController {
+
+    public String page(String page) {
+        return page;
+    }
+
+}
