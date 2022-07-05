10dc4df7d5d94bcccaca0a2f2b4adb40ca38fcf5
成龙生 <1536463948@qq.com>
2022-07-05 08:09:57 +0800

    过滤器

---
+++ QQZone/src/main/java/person/cls/qqzone/myssm/filter/CharacterEncodingFilter.java
@@ -0,0 +1,30 @@
+package person.cls.qqzone.myssm.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * @description: 编码设置过滤器
+ * @author: CLS
+ * @date: 2022-07-03-16:15
+ * @version: 1.0
+ */
+@WebFilter("*.do")
+public class CharacterEncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
---
+++ QQZone/src/main/java/person/cls/qqzone/myssm/filter/OpenSessionInViewFilter.java
@@ -0,0 +1,43 @@
+package person.cls.qqzone.myssm.filter;
+
+import person.cls.qqzone.myssm.trans.TransactionManager;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * @description: 事务管理过滤器
+ * @author: CLS
+ * @date: 2022-07-03-16:16
+ * @version: 1.0
+ */
+@WebFilter("*.do")
+public class OpenSessionInViewFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            TransactionManager.beginTransaction();
+            chain.doFilter(request, response);
+            TransactionManager.commitTransaction();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                TransactionManager.rollbackTransaction();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
