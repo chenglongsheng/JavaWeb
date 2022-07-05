9c20b80f3b3c39328ea483e36ae8c66699cc2132
成龙生 <1536463948@qq.com>
2022-07-05 08:11:35 +0800

    qqzone的pojo

---
+++ QQZone/src/main/java/person/cls/qqzone/pojo/Reply.java
@@ -0,0 +1,71 @@
+package person.cls.qqzone.pojo;
+
+import java.util.Date;
+
+/**
+ * @description: 回复表
+ * @author: CLS
+ * @date: 2022-07-03-10:44
+ * @version: 1.0
+ */
+public class Reply {
+
+    private Integer id;
+    private String content;
+    private Date replyDate;
+    private UserBasic author;
+    private Topic topic;
+
+    private HostReply hostReply;
+
+    public Reply() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getReplyDate() {
+        return replyDate;
+    }
+
+    public void setReplyDate(Date replyDate) {
+        this.replyDate = replyDate;
+    }
+
+    public UserBasic getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(UserBasic author) {
+        this.author = author;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+
+    public HostReply getHostReply() {
+        return hostReply;
+    }
+
+    public void setHostReply(HostReply hostReply) {
+        this.hostReply = hostReply;
+    }
+}
